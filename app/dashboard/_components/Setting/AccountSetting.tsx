330d5ad914b2084e1d6bb32a23b469850e4d38e6
saipepu <saipepu.mdy257@gmail.com>
2025-02-06 13:47:33 +0700

    update 1 file and create 1 file

--- app/dashboard/_components/Setting/AccountSetting.tsx
+++ app/dashboard/_components/Setting/AccountSetting.tsx
@@ -4,6 +4,8 @@ import { signOut, useSession } from 'next-auth/react'
 import { switchToAdminMode } from '@/app/api/user/switchToAdminMode'
 import { switchToUserMode } from '@/app/api/user/switchToUserMode'
 import React from 'react'
+import { Input } from '@/components/ui/input'
+import { Eye, EyeOff } from 'lucide-react'
 
 const AccountSetting = () => {
 
@@ -11,27 +13,6 @@ const AccountSetting = () => {
 
   const handleLogout = async () => {
     localStorage.removeItem('life.au-token')
-  }
-
-
-  const handleSwitchMode = async ({ mode } : { mode: string}) => {
-    console.log('handleSwitchMode', mode);
-    let dto = {
-      userId: session?.lifeAuUser._id,
-      secret: 'chayapol@life-au.live'
-    }
-    let response = null
-    if(mode === "admin") {
-      response = await switchToAdminMode(dto)  
-    } else {
-      response = await switchToUserMode(dto)
-    }
-
-    if(response.success) {
-      console.log(response);
-    } else {
-      console.log(response.message, "response.message")
-    }
   }
 
   return (
@@ -49,36 +30,7 @@ const AccountSetting = () => {
             {session?.user?.name || undefined}
           </p>
           {session?.lifeAuUser?.mode === 'admin' ? (
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button className="px-4 h-full w-[100px] bg-transparent border-l-2 flex justify-start items-center hover:bg-stone-200 hover:dark:bg-stone-500 rounded-none duration-300">
-                  <p className="text-base font-semibold text-stone-500 dark:text-stone-200">
-                    Activate
-                  </p>
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>🔑 Ready to activate the "User" mode?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    You will be logout after the activation and will have to login again.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel className='h-8'>Cancel</AlertDialogCancel>
-                  <AlertDialogAction
-                    className='className="p-2 bg-green-500 dark:text-white font-semibold h-8 rounded-md flex justify-center items-center'
-                    onClick={() => {
-                      handleSwitchMode({ mode: "user" })
-                      handleLogout()
-                      signOut({ redirectTo: '/landing'})
-                    }}
-                  >
-                    Continue
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
+            <ActivationAlertModel handleLogout={handleLogout} mode="user"/>
           ) : (
             <div className="mr-2 px-3 rounded-md bg-green-500  flex justify-center items-center">
               <p className="text-base font-semibold text-white dark:text-stone-200">
@@ -98,36 +50,7 @@ const AccountSetting = () => {
             </p>
           </div>
           {session?.lifeAuUser?.mode === 'user' ? (
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-              <Button className="px-4 h-full w-[100px] bg-transparent border-l-2 flex justify-start items-center hover:bg-stone-200 hover:dark:bg-stone-500 rounded-none duration-300">
-                  <p className="text-base font-semibold text-stone-500 dark:text-stone-200">
-                    Activate
-                  </p>
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>🔑 Ready to activate the "Admin" mode?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    You will be logout after the activation and will have to login again.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel className='h-8'>Cancel</AlertDialogCancel>
-                  <AlertDialogAction
-                    className='className="p-2 bg-green-500 dark:text-white font-semibold h-8 rounded-md flex justify-center items-center'
-                    onClick={() => {
-                      handleSwitchMode({ mode: "admin" })
-                      handleLogout()
-                      signOut({ redirectTo: '/landing'})
-                    }}
-                  >
-                    Continue
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
+            <ActivationAlertModel handleLogout={handleLogout} mode="admin"/>
           ) : (
             <div className="mr-2 px-3 rounded-md bg-green-500  flex justify-center items-center">
               <p className="text-base font-semibold text-white dark:text-stone-200">
@@ -173,6 +96,119 @@ const AccountSetting = () => {
         </AlertDialog>
       </div>
     </div>
+  )
+}
+
+const ActivationAlertModel = ({
+  handleLogout,
+  mode
+}: {
+  handleLogout: () => void,
+  mode: string
+}) => {
+
+  const { data: session } = useSession()
+  const [secret, setSecret] = React.useState('')
+  const [isSecretVisible, setIsSecretVisible] = React.useState(false)
+  const [isSecretValid, setIsSecretValid] = React.useState(false)
+  const [isSecretError, setIsSecretError] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const handleSwitchMode = async ({ mode } : { mode: string}) => {
+    console.log(secret)
+    if(secret.length < 6) {
+      setIsSecretError(true)
+      return
+    }
+    let dto = {
+      userId: session?.lifeAuUser._id,
+      secret: secret
+    }
+    let response = null
+    if(mode === "admin") {
+      response = await switchToAdminMode(dto)  
+    } else {
+      response = await switchToUserMode(dto)
+    }
+
+    if(response.success) {
+      console.log(response);
+      handleLogout()
+      signOut({ redirectTo: '/landing'})
+      setIsOpen(false)
+    } else {
+      console.log(response.message, "response.message")
+      setIsSecretError(true)
+    }
+  }
+
+  return (
+    <AlertDialog open={isOpen} >
+      <AlertDialogTrigger asChild>
+        <Button className="px-4 h-full w-[100px] bg-transparent border-l-2 flex justify-start items-center hover:bg-stone-200 hover:dark:bg-stone-500 rounded-none duration-300" onClick={() => setIsOpen(true)}>
+          <p className="text-base font-semibold text-stone-500 dark:text-stone-200">
+            Activate
+          </p>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>🔑 Ready to activate the "User" mode?</AlertDialogTitle>
+          <div className='w-full flex flex-col justify-start items-start gap-2'>
+            <p className="text-sm text-stone-500 dark:text-stone-200">
+              You will be logout after the activation and will have to login again.
+            </p>
+            <p className="text-lg font-semibold mt-5 text-black dark:text-white">
+              Enter your secret key to confirm the action.
+            </p>
+            <div className="w-full flex justify-start items-center gap-2">
+              <Input
+                type={isSecretVisible ? 'text' : 'password'}
+                className={`bg-white dark:bg-stone-950 bg-transparent ${isSecretError ? 'border-red-500' : 'border-input'} border rounded-md h-10 px-3`}
+                placeholder="••••••"
+                value={secret}
+                onChange={(e) => {
+                  setSecret(e.target.value)
+                  setIsSecretError(false)
+                  setIsSecretValid(e.target.value.length > 5)
+                }}
+                autoComplete='nope'
+              />
+              {isSecretVisible ? (
+                <Eye
+                  size={20}
+                  className='cursor-pointer'
+                  onClick={() => setIsSecretVisible(!isSecretVisible)}
+                />
+              ) : (
+                <EyeOff
+                  size={20}
+                  className='cursor-pointer'
+                  onClick={() => setIsSecretVisible(!isSecretVisible)}
+                />
+              )}
+            </div>
+            <p className="text-sm text-red-500 dark:text-red-400">
+              {isSecretError ? 'Invalid secret key.' : ''}
+            </p>
+            <p className="text-sm text-stone-500 dark:text-stone-200">
+              {isSecretValid ? '' : 'Secret key must be at least 6 characters.'}
+            </p>
+          </div>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className='h-8' onClick={() => setIsOpen(false)}>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className='className="p-2 bg-green-500 dark:text-white font-semibold h-8 rounded-md flex justify-center items-center'
+            onClick={() => {
+              handleSwitchMode({ mode: mode })
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
---
+++ app/landing/loading.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="relative w-full h-full flex flex-col justify-start items-center p-5">
+
+      {/* redirecting */}
+      <div className="absolute top-0 left-0 w-full h-full bg-white dark:bg-black flex justify-center items-center">
+        <div className="absolute w-14 h-14  border-dashed border border-black rounded-full animate-ping"></div>
+        <div className="absolute w-14 h-14  border-dashed border border-black rounded-full animate-spin"></div>
+        <div className='relative flex justify-center items-center'>
+          <svg width="26" height="24" viewBox="0 0 32 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_1251_384)">
+            <path fillRule="evenodd" clipRule="evenodd" d="M24.488 27.5051C25.2054 28.7501 26.5345 29.5172 27.973 29.5172C31.0656 29.5172 33.0005 26.1769 31.4585 23.5002L19.1666 2.1674C17.4317 -0.843503 13.0289 -0.685387 11.5152 2.44216L1.41376 23.313C-1.47653 29.1815 0.183116 29.5172 5.31219 29.5172H8.33569C11.6661 29.5172 13.7496 25.9198 12.0884 23.0376L8.09828 16.115C6.9254 14.0801 7.62673 11.4811 9.66474 10.31C11.7028 9.13887 14.3057 9.83914 15.4786 11.874L24.488 27.5051Z" fill='currentColor'
+              className="text-black dark:text-white"
+            />
+            </g>
+            <defs>
+            <clipPath id="clip0_1251_384">
+            <rect width="32" height="29.5172" fill="white"/>
+            </clipPath>
+            </defs>
+          </svg>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default loading
