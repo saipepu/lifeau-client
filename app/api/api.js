4ebcb076405de0f9a1feb6946c071c1113b0d3b4
saipepu <saipepu.mdy257@gmail.com>
2025-02-06 09:42:45 +0700

    update api.js

--- app/api/api.js
+++ app/api/api.js
@@ -1,5 +1,7 @@
-// export const api = "http://localhost:57205/api";
-// export const socketApi = "http://localhost:57205";
-
 export const api = "http://localhost:4002/api";
 export const socketApi = "http://localhost:4002";
+export const queueApi = "http://localhost:4001";
+
+// export const api = "http://localhost:59097/api";
+// export const socketApi = "http://localhost:59097";
+// export const queueApi = "http://localhost:59321";
