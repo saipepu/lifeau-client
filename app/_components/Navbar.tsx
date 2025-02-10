b2153e444609dba1f7f4fa213d2f9c436a4535fe
saipepu <saipepu.mdy257@gmail.com>
2025-02-11 01:24:24 +0700

    update Navbar.tsx

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Laptop, Moon, Sun } from 'lucide-react'
+import { Laptop, Moon, Slash, Sun } from 'lucide-react'
 import React, { useContext, useEffect, useState } from 'react'
 import {
   DropdownMenu,
@@ -14,12 +14,15 @@ import {
 import { ThemeContext } from '@/utils/hooks/themeContext';
 import { getPublicProfile } from '@/app/api/profile/getPublicProfile';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
 
   const [activeTheme, setActiveTheme] = useState<string>('system');
   const { theme, setTheme } = useContext(ThemeContext)
   const { data: session } = useSession()
+  const path = usePathname()
+  const [subPage, setSubPage] = useState<string>('')
 
   const handleLogout = async () => {
     localStorage.removeItem('life.au-token')
@@ -49,6 +52,14 @@ const Navbar = () => {
       mediaQuery.removeEventListener("change", handleSystemThemeChange);
     };
   }, []);
+
+  useEffect(() => {
+    if(path.includes('project')) {
+      setSubPage(path.split('/')[3].split('--of-')[0])
+    } else {
+      setSubPage('')
+    }
+  }, [path])
 
   const applyTheme = (theme: string) => {
     if (theme === 'dark') {
@@ -83,29 +94,37 @@ const Navbar = () => {
 
   return (
     <div className='w-full max-w-[1500px] px-5 py-4 flex justify-between items-center'>
-      <div
-        className='flex justify-start items-center gap-2 cursor-pointer'
-        onClick={() => window.location.href = '/landing'}
-      >
-        <div className='relative flex justify-center items-center pl-2'>
-          <svg width="26" height="24" viewBox="0 0 32 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0_1251_384)">
-            <path fillRule="evenodd" clipRule="evenodd" d="M24.488 27.5051C25.2054 28.7501 26.5345 29.5172 27.973 29.5172C31.0656 29.5172 33.0005 26.1769 31.4585 23.5002L19.1666 2.1674C17.4317 -0.843503 13.0289 -0.685387 11.5152 2.44216L1.41376 23.313C-1.47653 29.1815 0.183116 29.5172 5.31219 29.5172H8.33569C11.6661 29.5172 13.7496 25.9198 12.0884 23.0376L8.09828 16.115C6.9254 14.0801 7.62673 11.4811 9.66474 10.31C11.7028 9.13887 14.3057 9.83914 15.4786 11.874L24.488 27.5051Z" fill='currentColor'
-              className="text-black dark:text-white"
-            />
-            </g>
-            <defs>
-            <clipPath id="clip0_1251_384">
-            <rect width="32" height="29.5172" fill="white"/>
-            </clipPath>
-            </defs>
-          </svg>
+      <div className='flex justify-start items-center gap-2'>
+        <div
+          className='flex justify-start items-center gap-2 cursor-pointer'
+          onClick={() => window.location.href = '/landing'}
+        >
+          <div className='relative flex justify-center items-center pl-2'>
+            <svg width="26" height="24" viewBox="0 0 32 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clipPath="url(#clip0_1251_384)">
+              <path fillRule="evenodd" clipRule="evenodd" d="M24.488 27.5051C25.2054 28.7501 26.5345 29.5172 27.973 29.5172C31.0656 29.5172 33.0005 26.1769 31.4585 23.5002L19.1666 2.1674C17.4317 -0.843503 13.0289 -0.685387 11.5152 2.44216L1.41376 23.313C-1.47653 29.1815 0.183116 29.5172 5.31219 29.5172H8.33569C11.6661 29.5172 13.7496 25.9198 12.0884 23.0376L8.09828 16.115C6.9254 14.0801 7.62673 11.4811 9.66474 10.31C11.7028 9.13887 14.3057 9.83914 15.4786 11.874L24.488 27.5051Z" fill='currentColor'
+                className="text-black dark:text-white"
+              />
+              </g>
+              <defs>
+              <clipPath id="clip0_1251_384">
+              <rect width="32" height="29.5172" fill="white"/>
+              </clipPath>
+              </defs>
+            </svg>
+          </div>
+          <p className="text-xl font-semibold">Life.au</p>
         </div>
-        <p className="text-xl font-semibold">Life.au</p>
+        {subPage && (
+          <div className='flex justify-start items-center gap-2'>
+            <div className="w-[1px] h-6 bg-black dark:bg-white rotate-12"></div>
+            <p className="text-xl font-semibold">{subPage}</p>
+          </div>
+        )}
       </div>
       <div className='flex justify-center items-center gap-2'>
         <div className='flex justify-end items-center gap-2'>
-          {session?.lifeAuUser.mode === 'admin' && (
+          {session?.lifeAuUser?.mode === 'admin' && (
             <div className="mr-auto px-3 rounded-md bg-purple-200 dark:bg-purple-500 flex justify-center items-center">
               <p className="text-sm font-medium text-purple-500 dark:text-purple-200 font-mono">
                 Admin
