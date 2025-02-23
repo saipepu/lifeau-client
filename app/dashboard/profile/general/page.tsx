9ace737a583b9a1a011acdca74e21ac1ac36a67c
saipepu <saipepu.mdy257@gmail.com>
2025-02-23 14:28:17 +0700

    feat: create page.tsx

---
+++ app/dashboard/profile/general/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Tabbar from '@/app/_components/Tabbar'
+import React from 'react'
+import SettingView from './_components/SettingView'
+
+const page = () => {
+
+  const tabs = ['All Repositories', 'General']
+
+  return (
+    <div className='w-full h-full flex flex-col justify-start items-center'>
+      <div className='w-full flex flex-col justify-start items-center gap-1 bg-white dark:bg-stone-950 border-b dark:border-stone-700'>
+        <Tabbar tabs={tabs} />
+      </div>
+      <div className='w-full flex-1 flex flex-col justify-start items-center gap-1 bg-transparent'>
+        <SettingView />
+      </div>
+    </div>
+  )
+}
+
+export default page
