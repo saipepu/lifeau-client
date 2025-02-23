9edcfa49306403d1e07d8bc8304e3aeb8c29af27
saipepu <saipepu.mdy257@gmail.com>
2025-02-23 14:27:55 +0700

    update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,13 +9,13 @@ import Searchbar from './_components/Searchbar'
 import RepoCard from './_components/RepoCard'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
-import SettingView from './_components/Setting/SettingView';
+import SettingView from './profile/general/_components/SettingView';
 
 const Dashbaord = () => {
 
   const router = useRouter();
   const tabs = ['My Repositories', 'Settings']
-  const adminTabs = ['All Repositories', 'Settings']
+  const adminTabs = ['All Repositories', 'Resources', 'Settings']
   const [selectedTab, setSelectedTab] = useState<string>(tabs[0]);
   const [userContainers, setUserContainers] = useState<any[]>([]);
   const [allUsersContainers, setAllUsersContainers] = useState<any[]>([]);
@@ -59,33 +59,17 @@ const Dashbaord = () => {
   return (
     <>
       <div className='w-full flex flex-col justify-start items-center gap-1 bg-white dark:bg-stone-950 border-b dark:border-stone-700'>
-        <Tabbar selectedTab={selectedTab} setSelectedTab={setSelectedTab} tabs={session?.lifeAuUser?.mode === 'admin' ? adminTabs : tabs} numberOfRepositories={allUsersContainers.length.toString()}/>
+        <Tabbar tabs={session?.lifeAuUser?.mode === 'admin' ? adminTabs : tabs} numberOfRepositories={allUsersContainers.length.toString()}/>
       </div>
 
       {/* Main Content */}
-      {selectedTab === 'My Repositories' && (
-        <div className='w-full max-w-[1500px] flex-1 flex flex-col justify-start items-center gap-1 bg-transparent pb-10'>
-          <Searchbar />
-          <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 pt-0'>
-            {userContainers.map((container, index) => {
-              return (
-                <div key={index} className='col-span-1' onClick={() => router.push(`/dashboard/project/${container.name}--of-${userContainers.length}`)}>
-                  <RepoCard container={container} adminView={false} />
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      )}
-
-      {/* Main Content */}
       {selectedTab === 'All Repositories' && (
-        <div className='w-full max-w-[1500px] flex-1 flex flex-col justify-start items-center gap-1 bg-transparent pb-10'>
+        <div className='w-full max-w-[1600px] flex-1 flex flex-col justify-start items-center gap-1 bg-transparent pb-10'>
           <Searchbar />
           <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 pt-0'>
             {allUsersContainers.map((container, index) => {
               return (
-                <div key={index} className='col-span-1' onClick={() => router.push(`/dashboard/project/${container.name}--of-${allUsersContainers.length}`)}>
+                <div key={index} className='col-span-1' onClick={() => router.push(`/dashboard/project/${container?.name}--of-${allUsersContainers.length}`)}>
                   <RepoCard container={container} adminView={true} />
                 </div>
               )
@@ -94,14 +78,56 @@ const Dashbaord = () => {
         </div>
       )}
 
+      {/* Resources */}
+      {selectedTab === 'Resources' && (
+        <Resources />
+      )}
+
       {/* Settings */}
       {selectedTab === 'Settings' && (
         <SettingView />
       )}
 
-
-
     </>
+  )
+}
+
+const Resources = () => {
+
+  const { data: session } = useSession();
+  const tabs = ['K8s Deployments', 'K8s Services', 'K8s Ingresses']
+  const [selectedTab, setSelectedTab] = useState(tabs[0])
+
+  return (
+    <div className='w-full flex-1 flex flex-col justify-start items-center bg-transparent'>
+      <div className='w-full flex justify-center items-center gap-2 bg-white dark:bg-stone-950 border-b dark:border-stone-700'>
+        <div className='w-full max-w-[1600px] p-5 py-8 flex justify-start items-center gap-2'>
+          <h1 className='pl-2 text-2xl font-semibold text-black dark:text-white text-left'>{session?.lifeAuUser?.name} • Resources</h1>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[1600px] h-full flex justify-center items-start bg-transparent">
+        <div className="w-full h-full max-w-[15%] hidden md:flex flex-col gap-3 justify-start items-start p-5">
+          {tabs.map((tab, index) => {
+            return (
+              <p
+                key={index}
+                className={`pl-2 text-base hover:scale-105 duration-300 cursor-pointer ${selectedTab === tab ? "text-black dark:text-white" : "text-stone-400 dark:text-stone-500"}`}
+                onClick={() => setSelectedTab(tab)}
+              >
+                {tab}
+              </p>
+            );
+          })}
+        </div>
+
+        <div className="w-full min-h-full h-fit flex flex-col justify-start items-start p-5 gap-10 border-l-[1px] border-gray-200 dark:border-stone-700">
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
