2b8920e3e059edf1dfc12eafd8f0797507e91cc5
saipepu <saipepu.mdy257@gmail.com>
2025-02-23 15:23:07 +0700

    update Sparkle.tsx

--- app/landing/_components/Sparkle.tsx
+++ app/landing/_components/Sparkle.tsx
@@ -28,8 +28,8 @@ export function SparklesPreview() {
     <div className="h-[40rem] w-fullflex flex-col items-center justify-center rounded-md">
       <div className="w-[40rem] h-40 relative">
         {/* Gradients */}
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-md" />
+        <div className="absolute inset-x-20 top-0 left-[50%] -translate-x-[50%] bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-2/4 md:w-3/4" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
