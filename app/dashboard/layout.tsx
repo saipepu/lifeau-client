5bb7fd585a25994f53bdfea7db4640fce376e61c
saipepu <saipepu.mdy257@gmail.com>
2025-02-23 14:27:52 +0700

    update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import Footer from '../_components/Footer'
 import Navbar from '../_components/Navbar'
-import Tabbar from '../_components/Tabbar'
 
 const layout = ({
   children
@@ -12,13 +11,13 @@ const layout = ({
   const [selectedTab, setSelectedTab] = React.useState('Repositories')
 
   return (
-    <div className='w-full min-h-full h-full bg-stone-100 dark:bg-black overflow-hidden overflow-y-scroll flex flex-col justify-start items-center'>
+    <div className='w-full h-full bg-stone-100 dark:bg-black overflow-hidden overflow-y-scroll flex flex-col justify-start items-center'>
       
       <div className='w-full flex flex-col justify-start items-center gap-1 bg-white dark:bg-stone-950'>
         <Navbar />
       </div>
 
-      <div className='w-full flex-1 flex flex-col justify-start items-center bg-transparent pb-10'>
+      <div className='w-full flex-grow flex flex-col justify-start items-center bg-transparent'>
         {children}
       </div>
 
