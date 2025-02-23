3716708848b41e596b24ee85d0849fc386d765c5
saipepu <saipepu.mdy257@gmail.com>
2025-02-23 14:27:49 +0700

    update api.js

--- app/api/api.js
+++ app/api/api.js
@@ -1,6 +1,7 @@
 export const api = process.env.NEXT_PUBLIC_CONSUMER_API_URL;
 export const socketApi = process.env.NEXT_PUBLIC_SOCKET_API_URL;
 export const queueApi = process.env.NEXT_PUBLIC_PRODUCER_API_URL;
+export const k8Api = process.env.NEXT_PUBLIC_K8S_API_URL;
 
 // export const api = "http://localhost:59097/api";
 // export const socketApi = "http://localhost:59097";
