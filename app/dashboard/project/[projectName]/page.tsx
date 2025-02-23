d247aaba6c1ca45df51ae551a06aee6685d2c529
saipepu <saipepu.mdy257@gmail.com>
2025-02-23 14:28:31 +0700

    update page.tsx

--- app/dashboard/project/[projectName]/page.tsx
+++ app/dashboard/project/[projectName]/page.tsx
@@ -10,12 +10,16 @@ import { deleteContainer } from '@/app/api/container/deleteContainer';
 import { Button } from '@/components/ui/button';
 import { ArrowLeftIcon, Loader } from 'lucide-react';
 import { useSession } from 'next-auth/react';
-import { queueApi } from '@/app/api/api';
+import { api, k8Api, queueApi } from '@/app/api/api';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
+import { Input } from '@/components/ui/input';
 
 const page = () => {
 
   const { projectName } = useParams()
+  const { data: session } = useSession();
+  const router = useRouter();
+  const path = usePathname();
   const tabs = ['My Repositories', 'Overview', 'Settings']
   const adminTabs = ['All Repositories', 'Overview', 'Settings']
   const [selectedTab, setSelectedTab] = React.useState('Overview');
@@ -23,9 +27,9 @@ const page = () => {
   const [project, setProject] = useState<any>({});
   const [loading, setLoading] = useState<boolean>(false);
   const [message, setMessage] = useState<string>("");
-  const { data: session } = useSession();
-  const router = useRouter();
-  const path = usePathname();
+  const [confirmedProjectName, setConfirmedProjectName] = useState<string>("");
+  const [deployedLink, setDeployedLink] = useState<string>("");
+
   
   const GetOneContainer = async () => {
     const response = await getOneContainer({ name: projectName?.toString().split('--of-')[0] })
@@ -36,7 +40,6 @@ const page = () => {
     }
   }
 
-  console.log(project)
   const DeleteContainer = async () => {
     const response = await deleteContainer({ name: projectName?.toString().split('--of-')[0] })
     if(response.success) {
@@ -46,7 +49,7 @@ const page = () => {
     }
   }
 
-  const handleSubmit = async () => {
+  const handleBuild = async () => {
 
     setLoading(true);
     setMessage("It may take a few minutes to deploy your project. Please wait...");
@@ -71,7 +74,7 @@ const page = () => {
 
     if(response.success) {
       console.log(response);
-      setMessage("Your project has been pushed to DockerHub successfully!");
+      setMessage("Building in Process. Please wait...");
     }
     else {
       console.error(response);
@@ -88,6 +91,49 @@ const page = () => {
 
   const handleDelete = async () => {
     DeleteContainer();
+  }
+
+  const handleK8Deploy = async () => {
+
+    if(project?.name !== confirmedProjectName) {
+      setMessage("Project name does not match. Please try again.");
+      return
+    }
+    const projectName = project?.name.split('.').join('-')
+    setLoading(true);
+    setMessage("It may take a few minutes to deploy your project. Please wait...");
+    if(!project?.githubUrl && !session?.lifeAuUser._id) {
+      window.alert("Error: Please wait")
+      return "Error: Missing required fields"
+    }
+    const response = await fetch(`${k8Api}/initialize`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: projectName,
+        image: 'u6511923/'+project?.name,
+        port: 3000,
+      }),
+    }).then((res) => res.json())
+
+    if(response.success) {
+      console.log(response);
+      let res = "Kubernetes Deployment Successfull! 🚀 App link => http://pitchtrack-client.life-au.live"
+      let link = res.split('=>')[1].trim()
+      setDeployedLink(link)
+      setMessage("Your project has been deployed successfully!");
+    }
+    else {
+      console.log(response);
+      setMessage("Error: Deployment failed. Please try again.");
+    }
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
   }
 
   useEffect(() => {
@@ -120,41 +166,34 @@ const page = () => {
       {selectedTab === "Overview" && (
         <div className='w-full flex-1 flex flex-col justify-start items-center pb-10 gap-5'>
           <div className='w-full flex justify-center items-start gap-2 bg-white dark:bg-stone-950 border-b dark:border-stone-700'>
-            <div className='w-full max-w-[1500px] p-5 py-8 flex justify-start items-center gap-2'>
+            <div className='w-full max-w-[1600px] p-5 py-8 flex justify-start items-center gap-2'>
               <h1 className='pl-2 text-2xl font-semibold text-black dark:text-white text-left'>{projectName?.toString().split('--of-')[0]} • Overview</h1>
               <div className={`
                   font-semibold p-1 px-2 text-xs rounded-full mr-auto h-full flex justify-center items-center
-                  ${project?.status === "DEPLOYED" && "text-green-500 bg-green-200 dark:opacity-90"}
+                  ${project?.status === "READY" && "text-green-500 bg-green-200 dark:opacity-90"}
                   ${project?.status === "PENDING" && "text-yellow-500 bg-yellow-200 dark:opacity-90"}
                   ${project?.status === "FAILED" && "text-red-500 bg-red-200 dark:opacity-90"}
+                  ${project?.status === "DEPLOYED" && "text-purple-500 bg-purple-200 dark:opacity-90"}
                   `}>
                   {project?.status}
               </div>
               <div className='flex justify-end items-center gap-2'>
-                <Button className='p-2 bg-black dark:bg-white h-8 rounded-md flex justify-center items-center' onClick={() => handleSubmit()}>
-                  <p className='text-sm text-white dark:text-black whitespace-nowrap'>
-                    Reject
-                  </p>
-                </Button>
-                <Button className='p-2 bg-black dark:bg-white h-8 rounded-md flex justify-center items-center' onClick={() => handleSubmit()}>
-                  <p className='text-sm text-white dark:text-black whitespace-nowrap'>
-                    Approve
-                  </p>
-                </Button>
-                <Button className='p-2 bg-black dark:bg-white h-8 rounded-md flex justify-center items-center' onClick={() => handleSubmit()}>
-                  <p className='text-sm text-white dark:text-black whitespace-nowrap'>
-                    {loading ?
-                      <Loader size={20} className='animate-spin' />
-                    :
-                      "Redeploy"
-                    }
-                  </p>
-                </Button>
+                {session?.lifeAuUser?.mode !== 'admin' && (
+                  <Button className='p-2 bg-black dark:bg-white h-8 rounded-md flex justify-center items-center' onClick={() => handleBuild()}>
+                    <p className='text-sm text-white dark:text-black whitespace-nowrap'>
+                      {loading ?
+                        <Loader size={20} className='animate-spin' />
+                      :
+                        "Redeploy"
+                      }
+                    </p>
+                  </Button>
+                )}
               </div>
             </div>
           </div>
-          <div className='w-full max-w-[1500px] p-5 flex justify-start items-start gap-2'>
-            <div className="w-full max-h-[300px] border rounded-md p-2 flex flex-col justify-start items-start gap-2 bg-white dark:bg-stone-950">
+          <div className='w-full max-w-[1600px] p-5 flex flex-col justify-start items-start gap-5'>
+            <div className="w-full max-h-[300px] border rounded-md p-5 flex flex-col justify-start items-start gap-2 bg-white dark:bg-stone-950">
               <p className="text-sm font-semibold">Logs</p>
               <div className="w-full overflow-y-scroll flex flex-col-reverse justify-start items-start gap-2">
                 {logs.map((log, index) => (
@@ -166,6 +205,38 @@ const page = () => {
                 {logs.length === 0 && <p className="text-sm text-stone-500 leading-tight mt-1 whitespace-nowrap">No logs available</p>}
               </div>
             </div>
+            
+            <div className='w-full flex flex-col justify-start items-start gap-2'>
+              <p className="text-2xl text-purple-500 font-semibold">K8s Deployment</p>
+              <div className="w-full p-5 bg-white dark:bg-stone-950 rounded-md bdr cursor-pointer duration-300 flex flex-col justify-start items-start gap-2">
+                <p className="text-lg font-medium text-purple-500">Deploy to life-au.live Cluster</p>
+                <p className="text-sm text-stone-400 dark:text-stone-500">
+                  Please type in the project name <span className='font-semibold'>{project?.name}</span> to confirm the deployment.
+                </p>
+                <div className='w-full flex justify-start items-center gap-2'>
+                  <Input
+                    onChangeCapture={(e: any) => setConfirmedProjectName(e.target.value)}
+                    className="disabled:opacity-80"
+                  />
+                  <Button className='p-2 h-full bg-black dark:bg-white rounded-md flex justify-center items-center' onClick={() => handleK8Deploy()}>
+                    <p className='text-sm text-white dark:text-black whitespace-nowrap'>
+                      {loading ?
+                        <Loader size={20} className='animate-spin' />
+                      : project?.status === "DEPLOYED" ? "Redeploy" : "Deploy"
+                      }
+                    </p>
+                  </Button>
+                </div>
+                <p className="text-sm text-neutral-500">{message}</p>
+                {deployedLink && (
+                  <div className="p-2 px-4 text-xs rounded-lg mr-auto h-full flex justify-center items-center text-green-500 bg-green-200 dark:opacity-90">
+                    <p className="text-sm text-green-500">Whiplash 🔥 Your app is now live on </p>
+                    <a href={deployedLink} target="_blank" className="pl-2 text-black">{deployedLink}</a>
+                  </div>
+                )}
+              </div>
+            </div>
+
           </div>
         </div>
       )}
@@ -174,40 +245,11 @@ const page = () => {
         <div className='w-full flex-1 flex flex-col justify-start items-center pb-10 gap-5'>
           <div className='w-full flex-1 flex flex-col justify-start items-center pb-10 gap-5'>
             <div className='w-full flex justify-center items-start gap-2 bg-white dark:bg-stone-950 border-b dark:border-stone-700'>
-              <div className='w-full max-w-[1500px] p-5 py-8 flex justify-start items-center gap-2'>
+              <div className='w-full max-w-[1600px] p-5 py-8 flex justify-start items-center gap-2'>
                 <h1 className='pl-2 text-2xl font-semibold text-black dark:text-white text-left'>{projectName?.toString().split('--of-')[0]} • {selectedTab}</h1>
-                <div className={`
-                    font-semibold p-1 px-2 text-xs rounded-full mr-auto h-full flex justify-center items-center
-                    ${project?.status === "DEPLOYED" && "text-green-500 bg-green-200 dark:opacity-90"}
-                    ${project?.status === "PENDING" && "text-yellow-500 bg-yellow-200 dark:opacity-90"}
-                    ${project?.status === "FAILED" && "text-red-500 bg-red-200 dark:opacity-90"}
-                    `}>
-                    {project?.status}
-                </div>
-                <div className='flex justify-end items-center gap-2'>
-                  <Button className='p-2 bg-black dark:bg-white h-8 rounded-md flex justify-center items-center' onClick={() => handleSubmit()}>
-                    <p className='text-sm text-white dark:text-black whitespace-nowrap'>
-                      Reject
-                    </p>
-                  </Button>
-                  <Button className='p-2 bg-black dark:bg-white h-8 rounded-md flex justify-center items-center' onClick={() => handleSubmit()}>
-                    <p className='text-sm text-white dark:text-black whitespace-nowrap'>
-                      Approve
-                    </p>
-                  </Button>
-                  <Button className='p-2 bg-black dark:bg-white h-8 rounded-md flex justify-center items-center' onClick={() => handleSubmit()}>
-                    <p className='text-sm text-white dark:text-black whitespace-nowrap'>
-                      {loading ?
-                        <Loader size={20} className='animate-spin' />
-                      :
-                        "Redeploy"
-                      }
-                    </p>
-                  </Button>
-                </div>
               </div>
             </div>
-            <div className='w-full max-w-[1500px] p-5 flex flex-col justify-start items-start gap-2'>
+            <div className='w-full max-w-[1600px] p-5 flex flex-col justify-start items-start gap-2'>
               {/* danger zone */}
               <div className="w-full p-5 bg-white dark:bg-stone-950 rounded-md bdr cursor-pointer duration-300 flex flex-col justify-start items-start gap-2">
                 <p className="text-lg font-medium text-red-400">Danger Zone</p>
